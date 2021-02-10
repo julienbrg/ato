4b46f9e83d6db2a48b79b4a81b958403ccb5baa0
julienbrg <>
2021-02-10 16:00:43 +0100

    add .idea in gitignore

--- contracts/DAI.sol
+++ contracts/DAI.sol
@@ -7,13 +7,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract DAI is ERC20
 {
   constructor()
-  public ERC20("Magic DAI for hacerks", "DAI") { }
+  public ERC20("Magic DAI for hackers", "DAI")
+  {
+    _mint(msg.sender, 1000000*10**18);
+  }
 
   event Withdrawn(address user, uint amount );
 
   function withdraw()public
   {
-    _mint(msg.sender, 500000*10**18);
+    _mint(msg.sender, 50*10**18);
     emit Withdrawn (msg.sender, 50);
   }
 }
